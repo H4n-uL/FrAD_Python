3783489efa66dd8a8527850f9c7986e8900e586d
h4n-ul <jun061119@gmail.com>
2023-11-17 17:18:18 +0900

    ~DEPR/UPD$ :: Reserved 6 more ECC algorithms

--- Python/fva/tools/ecc.py
+++ Python/fva/tools/ecc.py
@@ -5,18 +5,30 @@ from reedsolo import RSCodec, ReedSolomonError
 class ecc:
     ENCODE_OPTIONS = {
         None: 0b000,
-        'digi': 0b001,
-        'rdsl': 0b010,
-        'af': 0b011,
-        'digitalfile': 0b001,
-        'reedsolomon': 0b010,
-        'advancedformat': 0b011,
+        'none': 0b000,
+        'hamming': 0b001, 'hamm': 0b001,
+        'golay': 0b010,
+        'convolutional': 0b011, 'conv': 0b011,
+        'reedsolomon': 0b100, 'rdsl': 0b100,
+        'bch': 0b101,
+        'lpdc': 0b110,
+        'turbo': 0b111,
+        0b000: 0b000,
         0b001: 0b001,
         0b010: 0b010,
         0b011: 0b011,
+        0b010: 0b100,
+        0b101: 0b101,
+        0b110: 0b110,
+        0b11: 0b111,
+        0: 0b000,
         1: 0b001,
         2: 0b010,
-        3: 0b011
+        3: 0b011,
+        4: 0b100,
+        5: 0b101,
+        6: 0b110,
+        7: 0b111
     }
 
     def split_data(data, chunk_size):
@@ -37,15 +49,15 @@ class ecc:
             print(f'Error: {e}')
 
     def encode(data, option):
-        if option is None: return data
-        elif ecc.ENCODE_OPTIONS[option] == 0b010: return ecc.rdsl.encode(data)
+        if ecc.ENCODE_OPTIONS[option] == 0b000: return data
+        elif ecc.ENCODE_OPTIONS[option] == 0b100: return ecc.rdsl.encode(data)
         else:
             print('Not supported yet')
             return data
 
     def decode(data_rs, option):
         if option == 0b000: return data_rs
-        elif option == 0b010: return ecc.rdsl.decode(data_rs)
+        elif option == 0b100: return ecc.rdsl.decode(data_rs)
         else:
             print('Not supported yet')
             return data_rs
