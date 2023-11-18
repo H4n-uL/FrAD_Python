c3366dd1071632cb26b97b0d25e58a40d8d04ae1
h4n-ul <jun061119@gmail.com>
2023-11-18 15:09:46 +0900

    #REFCT# :: Intergrated Decoder and Player

--- Python/fva/decoder.py
+++ Python/fva/decoder.py
@@ -1,17 +1,20 @@
-from .tools.ecc import ecc
+import binascii
+import hashlib
 from ml_dtypes import bfloat16
 import numpy as np
 from scipy.io import wavfile
 from scipy.fft import ifft
 import struct
+from .tools.ecc import ecc
 
 class decode:
     def mono(sample_rate, data, bits):
         data = data[:,0] * np.exp(1j * data[:,1])
+        wave = np.int32(np.real(ifft(data)))
 
-        if bits == 32: wave = np.int32(np.real(ifft(data)))
-        elif bits == 16: wave = np.int16(np.real(ifft(data / 2**16)))
-        elif bits == 8: wave = np.uint8(np.real(ifft(data / 2**24)))
+        if bits == 32: pass
+        elif bits == 16: wave = np.int16(wave / 2**16)
+        elif bits == 8: wave = np.uint8(wave / 2**24 + 2**7)
         else: raise ValueError(f"Illegal value {bits} for bits: only 8, 16, and 32 bits are available for decoding.")
 
         return wave
@@ -19,8 +22,6 @@ class decode:
     def stereo(sample_rate, data, bits):
         left_freq = data[:, 0] * np.exp(1j * data[:, 1])
         right_freq = data[:, 2] * np.exp(1j * data[:, 3])
-
-        left_wave = right_wave = None
 
         left_wave = np.int32(np.fft.ifft(left_freq).real)
         right_wave = np.int32(np.fft.ifft(right_freq).real)
@@ -35,8 +36,8 @@ class decode:
         else: raise ValueError(f"Illegal value {bits} for bits: only 8, 16, and 32 bits are available for decoding.")
 
         return np.column_stack((left_wave, right_wave))
-
-    def dec(file_path, out: str = None, bits: int = 32, correct_error: bool = False):
+    
+    def internal(file_path, bits: int = 32):
         with open(file_path, 'rb') as f:
             header = f.read(256)
 
@@ -50,41 +51,55 @@ class decode:
             cb = cfb >> 3
             fb = cfb & 0b111
             is_ecc_on = True if (struct.unpack('<B', header[0x16:0x17])[0] >> 7) == 0b1 else False
+            checksum_header = header[0xf0:0x100]
 
             f.seek(header_length)
 
-            block = f.read()
+            data = f.read()
+            checksum_data = hashlib.md5(data).digest()
             if is_ecc_on == False:
-                pass
-            elif correct_error != True:
-                chunks = ecc.split_data(block, 128)
-                block =  b''.join([bytes(chunk[:112]) for chunk in chunks])
+                if checksum_data == checksum_header:
+                    pass
+                else:
+                    print(f'Checksum: on header[{checksum_header}] vs on data[{checksum_data}]')
+                    raise Exception('File has corrupted but it has no ECC option. Decoder halted.')
             else:
-                block = ecc.decode(block, is_ecc_on)
+                if checksum_data == checksum_header:
+                    chunks = ecc.split_data(data, 128)
+                    data =  b''.join([bytes(chunk[:112]) for chunk in chunks])
+                else:
+                    print(f'{file_path} has been corrupted, Please repack your file for the best music experience.')
+                    print(f'Checksum: on header[{checksum_header}] vs on data[{checksum_data}]')
+                    data = ecc.decode(data)
+
             # if b == 0b110:
-            #     block_data = np.frombuffer(block, dtype=np.float512)
+            #     data_numpy = np.frombuffer(data, dtype=np.float512)
             # elif b == 0b101:
-            #     block_data = np.frombuffer(block, dtype=np.float256)
+            #     data_numpy = np.frombuffer(data, dtype=np.float256)
             # elif b == 0b100:
-            #     block_data = np.frombuffer(block, dtype=np.float128)
+            #     data_numpy = np.frombuffer(data, dtype=np.float128)
             if fb == 0b011:
-                block_data = np.frombuffer(block, dtype=np.float64)
+                data_numpy = np.frombuffer(data, dtype=np.float64)
             elif fb == 0b010:
-                block_data = np.frombuffer(block, dtype=np.float32)
+                data_numpy = np.frombuffer(data, dtype=np.float32)
             elif fb == 0b001:
-                block_data = np.frombuffer(block, dtype=bfloat16)
+                data_numpy = np.frombuffer(data, dtype=bfloat16)
             else:
                 raise Exception('Illegal bits value.')
 
             if cb == 2:
-                block_data = block_data.reshape(-1, 4)
-                restored = decode.stereo(sample_rate, block_data, bits)
+                data_numpy = data_numpy.reshape(-1, 4)
+                restored = decode.stereo(sample_rate, data_numpy, bits)
             elif cb == 1:
-                block_data = block_data.reshape(-1, 2)
-                restored = decode.mono(sample_rate, block_data, bits)
+                data_numpy = data_numpy.reshape(-1, 2)
+                restored = decode.mono(sample_rate, data_numpy, bits)
             else:
                 raise Exception('Fourier Analogue only supports Mono and Stereo.')
+            
+            return restored, sample_rate
 
-            if out is not None and out[-4:-1]+out[-1] != '.wav':
-                out += '.wav'
-            wavfile.write(out if out is not None else'restored.wav', sample_rate, restored)
+    def dec(file_path, out: str = None, bits: int = 32):
+        restored, sample_rate = decode.internal(file_path, bits)
+        if out is not None and out[-4:-1]+out[-1] != '.wav':
+            out += '.wav'
+        wavfile.write(out if out is not None else'restored.wav', sample_rate, restored)
--- Python/fva/player.py
+++ Python/fva/player.py
@@ -1,56 +1,8 @@
 from .decoder import decode
-from .tools.ecc import ecc
-from ml_dtypes import bfloat16
-import numpy as np
-import struct
 import sounddevice as sd
 
 class player:
-    def play(file_path, correct_error: bool = False):
-        with open(file_path, 'rb') as f:
-            header = f.read(256)
-
-            signature = header[0x0:0xa]
-            if signature != b'\x7e\x8b\xab\x89\xea\xc0\x9d\xa9\x68\x80':
-                raise Exception('This is not Fourier Analogue file.')
-            header_length = struct.unpack('<Q', header[0xa:0x12])[0]
-            sample_rate = int.from_bytes(header[0x12:0x15], 'little')
-            cfb = struct.unpack('<B', header[0x15:0x16])[0]
-            channels = cfb >> 3
-            bits = cfb & 0b111
-            is_ecc_on = True if (struct.unpack('<B', header[0x16:0x17])[0] >> 7) == 0b1 else False
-
-            f.seek(header_length)
-
-            block = f.read()
-            if is_ecc_on == False:
-                pass
-            elif correct_error != True:
-                chunks = ecc.split_data(block, 128)
-                block =  b''.join([bytes(chunk[:112]) for chunk in chunks])
-            else:
-                block = ecc.decode(block, is_ecc_on)
-            # if bits == 0b110:
-            #     block_data = np.frombuffer(block, dtype=np.float512)
-            # elif bits == 0b101:
-            #     block_data = np.frombuffer(block, dtype=np.float256)
-            # elif bits == 0b100:
-            #     block_data = np.frombuffer(block, dtype=np.float128)
-            if bits == 0b011:
-                block_data = np.frombuffer(block, dtype=np.float64)
-            elif bits == 0b010:
-                block_data = np.frombuffer(block, dtype=np.float32)
-            elif bits == 0b001:
-                block_data = np.frombuffer(block, dtype=bfloat16)
-            else:
-                raise Exception('Illegal bits value.')
-
-            if channels == 2:
-                block_data = block_data.reshape(-1, 4)
-                wave = decode.stereo(sample_rate, block_data, 32)
-            if channels == 1:
-                block_data = block_data.reshape(-1, 2)
-                wave = decode.mono(sample_rate, block_data, 32)
-
-            sd.play(wave, samplerate=sample_rate)
-            sd.wait()
+    def play(file_path):
+        restored, sample_rate = decode.internal(file_path, 32)
+        sd.play(restored, samplerate=sample_rate)
+        sd.wait()
