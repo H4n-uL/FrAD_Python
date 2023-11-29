824b572c25f64d13c3de319646ec0b85b9f11f2f
h4n-ul <jun061119@gmail.com>
2023-11-30 08:16:39 +0900

    $UPDATE$ :: Updated speed modification param

--- Python/fra/player.py
+++ Python/fra/player.py
@@ -11,8 +11,14 @@ class player:
             time.sleep(1/18)
             print('\x1b[1A\x1b[2K', end='')
 
-    def play(file_path):
+    def play(file_path, keys: float = None, speed_in_times: float = None):
+        if keys and speed_in_times:
+            raise ValueError('Keys and Speed parameter cannot be set at the same time.')
         restored, sample_rate = decode.internal(file_path, 32)
+        if keys and not speed_in_times:
+            sample_rate *= 2**(keys/12)
+        elif not keys and speed_in_times:
+            sample_rate *= speed_in_times
         duration = len(restored) / sample_rate
 
         sd.play(restored, samplerate=sample_rate)
