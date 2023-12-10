d1dcab6c94cc776bcb02021e40582760109f4e19
h4n-ul <jun061119@gmail.com>
2023-12-11 00:40:11 +0900

    $UPDATE$ :: Updated CLI

--- Java/src/main/java/com/h4nul/fourieranalogue/FourierAnalogueApp.java
+++ Java/src/main/java/com/h4nul/fourieranalogue/FourierAnalogueApp.java
@@ -1,13 +1,88 @@
 package com.h4nul.fourieranalogue;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.cli.*;
+
 public class FourierAnalogueApp {
     public static void main(String[] args) throws Exception {
-        // Encode encode = new Encode();
-        Decode decode = new Decode();
+        Options options = new Options();
 
-        String fra = "Yasashisa_no_Kioku.fra";
-        String flac = "restored.flac";
-        // encode.enc("/Users/H4nUL/Desktop/cks.flac", 64, fra, false, null, null, null);
-        decode.dec(fra, flac, 32, "flac", null);
+        Option input = new Option("i", "input", true, "input file path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "output file path");
+        output.setRequired(false);
+        options.addOption(output);
+
+        Option bits = new Option("b", "bits", true, "output file path");
+        bits.setRequired(false);
+        options.addOption(bits);
+
+        Option applyEcc = new Option("ecc", "applyecc", false, "output file path");
+        applyEcc.setRequired(false);
+        options.addOption(applyEcc);
+
+        Option img = new Option("img", "image", true, "output file path");
+        img.setRequired(false);
+        options.addOption(img);
+
+        Option codec = new Option("c", "codec", true, "output file path");
+        codec.setRequired(false);
+        options.addOption(codec);
+
+        Option meta = Option.builder("m")
+                .longOpt("meta")
+                .desc("metadata in \"key\" \"value\" format")
+                .hasArgs()
+                .build();
+        options.addOption(meta);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("FourierAnalogueApp", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+        int bitsValue = Integer.parseInt(cmd.getOptionValue("bits"));
+        boolean isecc = cmd.hasOption("ecc");
+        String imageFilePath = cmd.getOptionValue("img");
+        String codecType = cmd.getOptionValue("codec");
+        String[] metaValues = cmd.getOptionValues("meta");
+
+        Map<String, byte[]> metaMap = new HashMap<>();
+        for (int i = 0; i < metaValues.length; i += 2) {
+            String key = metaValues[i];
+            byte[] value = metaValues[i+1].getBytes(StandardCharsets.UTF_8);
+            metaMap.put(key, value);
+        }
+
+        Path imagePath = Paths.get(imageFilePath);
+        byte[] imageBytes = Files.readAllBytes(imagePath);
+
+        if (args[0].equals("encode")) {
+            Encode encode = new Encode();
+            encode.enc(inputFilePath, bitsValue, outputFilePath, isecc, null, metaMap, imageBytes);
+        }
+        else if (args[0].equals("decode")) {
+            Decode decode = new Decode();
+            decode.dec(inputFilePath, outputFilePath, bitsValue, codecType, null);
+        }
     }
 }
