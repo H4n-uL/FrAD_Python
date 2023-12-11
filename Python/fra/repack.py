f78d5a51426b3c49bd6d26cf7786aed5f36c3bcd
h4n-ul <jun061119@gmail.com>
2023-12-11 11:36:31 +0900

    $UPDATE$ :: Adding ECC when ECC is not applied

--- Python/fra/repack.py
+++ Python/fra/repack.py
@@ -13,19 +13,21 @@ class repack:
 
             header_length = struct.unpack('<Q', head[0xa:0x12])[0]
             is_ecc_on = True if (struct.unpack('<B', head[0x16:0x17])[0] >> 7) == 0b1 else False
-            if not is_ecc_on:
-                return
 
             f.seek(header_length)
 
             block = f.read()
 
-            data = ecc.encode(ecc.decode(block), True)
+            if is_ecc_on:
+                block = ecc.decode(block)
+
+            data = ecc.encode(block, True)
             checksum = hashlib.md5(data).digest()
 
             f.seek(0)
             head = bytearray(f.read(header_length))
             head[0xf0:0x100] = checksum
+            head[0x16:0x17] = struct.pack('<B', 0b1 << 7)
             head = bytes(head)
 
             f.seek(0)
