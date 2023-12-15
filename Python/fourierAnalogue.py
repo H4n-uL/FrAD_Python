dce3afb21e3b2aab5e9a54de8fab811e5042f441
h4n-ul <jun061119@gmail.com>
2023-12-15 18:54:39 +0900

    *DEBUG* :: Fixed None input problem on CLI

--- Python/fourierAnalogue.py
+++ Python/fourierAnalogue.py
@@ -1,15 +1,9 @@
 import argparse
 import base64
 import json
-from fra import encode
-from fra import decode
-from fra import header
-from fra import player
-from fra import repack
 
 def main(action, args):
     input = args.input
-    output = args.output if args.output else 'fourierAnalogue.fra'
     meta = args.meta
     if args.jsonmeta is not None:
         with open(args.jsonmeta, 'r') as f:
@@ -30,10 +24,17 @@ def main(action, args):
             img = i.read()
 
     if action == 'encode':
-        encode.enc(input, int(args.bits), out=output, apply_ecc=args.ecc, new_sample_rate=int(args.nsr) if args.nsr is not None else None, meta=meta, img=img)
+        from fra import encode
+        output = args.output if args.output else 'fourierAnalogue.fra'
+        nsr = None if args.nsr == None else int(args.nsr)
+        encode.enc(input, int(args.bits), out=output, apply_ecc=args.ecc, new_sample_rate=nsr, meta=meta, img=img)
     elif action == 'decode':
-        decode.dec(input, out=output, bits=int(args.bits), codec=args.codec, quality=args.quality)
+        from fra import decode
+        bits = 32 if args.bits == None else int(args.bits)
+        codec = args.codec if args.codec is not None else 'flac'
+        decode.dec(input, out=args.output, bits=bits, codec=codec, quality=args.quality)
     elif action == 'parse':
+        from fra import header
         output = args.output if args.output is not None else 'metadata'
         head, img = header.parse(input)
         result_list = []
@@ -57,11 +58,14 @@ def main(action, args):
         with open(output+'.meta.json', 'w') as m: m.write(json.dumps(result_list, ensure_ascii=False))
         with open(output+'.meta.image', 'wb') as m: m.write(img)
     elif action == 'modify' or action == 'meta-modify':
+        from fra import header
         print(meta)
         header.modify(input, meta=meta, img=img)
     elif action == 'ecc':
+        from fra import repack
         repack.ecc(input)
     elif action == 'play':
+        from fra import player
         player.play(input, keys=int(args.keys) if args.keys is not None else None, speed_in_times=float(args.speed) if args.speed is not None else None)
     else:
         raise ValueError("Invalid action. Please choose 'encode' or 'decode'.")
@@ -69,18 +73,18 @@ def main(action, args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fourier Analogue Codec')
     parser.add_argument('action', choices=['encode', 'decode', 'parse', 'modify', 'meta-modify', 'ecc', 'play'], help='action to perform')
-    parser.add_argument('-i', '--input', required=True, help='input file path')
-    parser.add_argument('-o', '--output', required=False, help='output file path')
-    parser.add_argument('-b', '--bits', required=False, help='output file bit depth')
-    parser.add_argument('-n', '--nsr', required=False, help='resample as new sample rate')
-    parser.add_argument('-img', '--image', required=False, help='image file path')
-    parser.add_argument('-c', '--codec', required=False, help='codec type')
-    parser.add_argument('-e', '--ecc', action='store_true', help='enable ecc')
-    parser.add_argument('-s', '--speed', required=False, help='play speed(in times)')
-    parser.add_argument('-q', '--quality', required=False, help='decode quality')
-    parser.add_argument('-k', '--keys', required=False, help='keys')
-    parser.add_argument('-m', '--meta', action='append', nargs=2, required=False, help='metadata in "key" "value" format')
-    parser.add_argument('-jm', '--jsonmeta', required=False, help='metadata in json')
+    parser.add_argument('-i',   '--input',    required=True,        help='input file path')
+    parser.add_argument('-o',   '--output',   required=False,       help='output file path')
+    parser.add_argument('-b',   '--bits',     required=False,       help='output file bit depth')
+    parser.add_argument('-n',   '--nsr',      required=False,       help='resample as new sample rate')
+    parser.add_argument('-img', '--image',    required=False,       help='image file path')
+    parser.add_argument('-c',   '--codec',    required=False,       help='codec type')
+    parser.add_argument('-e',   '--ecc',      action='store_true',  help='enable ecc')
+    parser.add_argument('-s',   '--speed',    required=False,       help='play speed(in times)')
+    parser.add_argument('-q',   '--quality',  required=False,       help='decode quality')
+    parser.add_argument('-k',   '--keys',     required=False,       help='keys')
+    parser.add_argument('-m',   '--meta',     action='append',      nargs=2, required=False, help='metadata in "key" "value" format')
+    parser.add_argument('-jm',  '--jsonmeta', required=False,       help='metadata in json')
     
     args = parser.parse_args()
     main(args.action, args)
