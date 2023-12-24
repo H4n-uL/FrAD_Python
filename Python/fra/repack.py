20d9fe547bc121aa789a87cb39a039e49ffe03b3
h4n-ul <jun061119@gmail.com>
2023-12-24 16:09:21 +0900

    $UPDATE$ :: Updated File header structure

--- Python/fra/repack.py
+++ Python/fra/repack.py
@@ -5,13 +5,14 @@ import struct
 
 class repack:
     def ecc(file_path):
-        with open(file_path, 'r+b') as f:
+        with open(file_path, 'rb') as f:
             head = f.read(256)
 
             methods.signature(head[0x0:0x3])
 
             header_length = struct.unpack('>Q', head[0x8:0x10])[0]
-            is_ecc_on = True if (struct.unpack('<B', head[0x16:0x17])[0] >> 7) == 0b1 else False
+            efb = struct.unpack('<B', head[0x10:0x11])[0]
+            is_ecc_on = True if (efb >> 4) == 0b1 else False
 
             f.seek(header_length)
 
@@ -26,8 +27,8 @@ class repack:
             f.seek(0)
             head = bytearray(f.read(header_length))
             head[0xf0:0x100] = checksum
-            head[0x16:0x17] = struct.pack('<B', 0b1 << 7)
+            head[0x10:0x11] = struct.pack('<B', 0b1 << 4 | efb)
             file = bytes(head) + data
 
-            f.seek(0)
+        with open(file_path, 'wb') as f:
             f.write(file)
