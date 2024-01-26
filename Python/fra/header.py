413a4ae9ba044e51a4b987b1c778eff2b4b2f0a4
h4n-ul <jun061119@gmail.com>
2024-01-26 19:55:02 +0900

    $UPDATE$ :: Minimalizing Header

--- Python/fra/header.py
+++ Python/fra/header.py
@@ -2,13 +2,6 @@ from .common import variables, methods
 import math, os, shutil, struct, sys
 from .tools.headb import headb
 
-b3_to_bits = {
-    0b100: 128,
-    0b011: 64,
-    0b010: 32,
-    0b001: 16
-}
-
 class header:
     def parse(file_path):
         d = list()
@@ -49,14 +42,7 @@ class header:
                 sample_rate = struct.unpack('>I', head[0x4:0x8])[0]    # 0x04-4B:       Sample rate
 
                 header_length = struct.unpack('>Q', head[0x8:0x10])[0] # 0x08-8B:       Total header size
-                efb = struct.unpack('<B', head[0x10:0x11])[0]          # 0x10:          ECC-Float Bit
-                is_cosine = True if (efb >> 7 & 0b1) == 0b1 else False # 0x10@0b111:    MDCT Toggle(Enabled if 1)
-                is_ecc_on = True if (efb >> 4 & 0b1) == 0b1 else False # 0x10@0b100:    ECC Toggle(Enabled if 1)
-                bits = b3_to_bits.get(efb & 0b111)                     # 0x10@0b011-3b: Stream bit depth
-                fsize = struct.unpack('<B', head[0x11:0x12])[0] >> 5   # 0x11@0b111-4b: Frame size
                 checksum_stream = head[0xf0:0x100]                     # 0xf0-16B:      Stream hash
-
-                fsize = int(math.pow(2, fsize + 7))
 
                 # Backing up audio data
                 f.seek(header_length)
@@ -64,7 +50,7 @@ class header:
                     temp.write(f.read())
 
             # Making new header
-            head_new = headb.uilder(sample_rate, channel, is_cosine, is_ecc_on, bits, checksum_stream,meta, img)
+            head_new = headb.uilder(sample_rate, channel, checksum_stream,meta, img)
 
             # Overwriting Fourier Analogue-in-Digital file
             with open(variables.temp, 'wb') as f: # DO NEVER DELETE THIS
