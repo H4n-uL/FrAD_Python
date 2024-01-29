e1f39b93fd5ac0f8d070e28d1c3bb800900cf0c6
h4n-ul <jun061119@gmail.com>
2024-01-30 01:56:43 +0900

    $UPDATE$ :: Updated temp directory

--- Python/fra/common.py
+++ Python/fra/common.py
@@ -5,32 +5,36 @@ from scipy.interpolate import interp1d
 class variables:
     hash_block_size = 2**20
 
-    dir = os.path.dirname(os.path.realpath(__file__))
-    temp = os.path.join(dir, f'temp.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.swv')
-    temp2 = os.path.join(dir, f'temp.ecc.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.swv')
-    temp_pcm = os.path.join(dir, f'temp.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.pcm')
-    temp2_pcm = os.path.join(dir, f'temp.rate.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.pcm')
-    temp_flac = os.path.join(dir, f'temp.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.flac')
-    meta = os.path.join(dir, f'{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.meta')
+    directory = os.path.dirname(os.path.realpath(__file__))
+    tmpdir = os.path.join(directory, 'tempfiles')
+    os.makedirs(dir, exist_ok=True)
+    temp =      os.path.join(tmpdir, f'temp.{  base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.swv')
+    temp =      os.path.join(tmpdir, f'temp.{  base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.swv')
+    temp2 =     os.path.join(tmpdir, f'temp.2.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.swv')
+    temp_pcm =  os.path.join(tmpdir, f'temp.{  base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.pcm')
+    temp2_pcm = os.path.join(tmpdir, f'temp.2.{base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.pcm')
+    temp_dsd =  os.path.join(tmpdir, f'temp.{  base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.bitstream')
+    temp_flac = os.path.join(tmpdir, f'temp.{  base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.flac')
+    meta =      os.path.join(tmpdir, f'{       base64.b64encode(secrets.token_bytes(64)).decode().replace("/", "_")}.meta')
 
     oper = platform.uname()
     arch = platform.machine().lower()
 
     if oper.system == 'Windows' and arch in ['amd64', 'x86_64']:
-        ffmpeg = os.path.join(dir, 'res', 'codec', 'ffmpeg.Windows')
-        aac = os.path.join(dir, 'res', 'codec', 'AppleAAC.Windows')
-        ffprobe = os.path.join(dir, 'res', 'parser', 'ffprobe.Windows')
+        ffmpeg =      os.path.join(directory, 'res', 'codec',  'ffmpeg.Windows')
+        aac =         os.path.join(directory, 'res', 'codec',  'AppleAAC.Windows')
+        ffprobe =     os.path.join(directory, 'res', 'parser', 'ffprobe.Windows')
     elif oper.system == 'Darwin':
-        ffmpeg = os.path.join(dir, 'res', 'codec', 'ffmpeg.macOS')
-        aac = 'afconvert'
-        ffprobe = os.path.join(dir, 'res', 'parser', 'ffprobe.macOS')
+        ffmpeg =      os.path.join(directory, 'res', 'codec',  'ffmpeg.macOS')
+        aac =         'afconvert'
+        ffprobe =     os.path.join(directory, 'res', 'parser', 'ffprobe.macOS')
     else:
         if arch in ['amd64', 'x86_64']:
-            ffmpeg = os.path.join(dir, 'res', 'codec', 'ffmpeg.AMD64')
-            ffprobe = os.path.join(dir, 'res', 'parser', 'ffprobe.AMD64')
+            ffmpeg =  os.path.join(directory, 'res', 'codec',  'ffmpeg.AMD64')
+            ffprobe = os.path.join(directory, 'res', 'parser', 'ffprobe.AMD64')
         if arch == 'arm64':
-            ffmpeg = os.path.join(dir, 'res', 'codec', 'ffmpeg.AArch64')
-            ffprobe = os.path.join(dir, 'res', 'parser', 'ffprobe.AArch64')
+            ffmpeg =  os.path.join(directory, 'res', 'codec',  'ffmpeg.AArch64')
+            ffprobe = os.path.join(directory, 'res', 'parser', 'ffprobe.AArch64')
 
 class methods:
     def signature(sign):
