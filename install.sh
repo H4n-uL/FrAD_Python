764fe7cb4737eac6f743d1b7ada1a3788d9ad0db
h4n-ul <jun061119@gmail.com>
2024-02-03 02:04:00 +0900

    $UPDATE$ :: Removing alias on deleting previous installation

--- install.sh
+++ install.sh
@@ -10,8 +10,16 @@ fi
 if [ -d "$FOURIER_PATH/FrAD-Codec" ]; then
     read -p "Fourier Analogue-in-Digital is already installed. Would you like to remove and reinstall? (Y/N) " yn
     case $yn in
-        [Yy]* ) rm -r $FOURIER_PATH/FrAD-Codec;;
-        * ) echo "Installation Aborted."; exit 1;;
+        [Yy]* )
+            rm -r $FOURIER_PATH/FrAD-Codec
+            for rcfile in `ls -a ~ | grep 'shrc$'`; do
+                sed -i '' "/FOURIER_PATH/d" ~/$rcfile
+                sed -i '' "/alias fourier=/d" ~/$rcfile
+            done
+            echo "Deleted.";;
+
+        * )
+            echo "Installation Aborted."; exit 1;;
     esac
 fi
 
@@ -25,12 +33,6 @@ mkdir -p $FOURIER_PATH/FrAD-Codec || { exit 1; }
 rsync -a --exclude='__pycache__' --exclude='.DS_Store' "$(dirname "$0")/src/" $FOURIER_PATH/FrAD-Codec || { echo "Failed copying files."; exit 1; }
 
 for rcfile in `ls -a ~ | grep 'shrc$'`; do
-    if grep -q 'FOURIER_PATH' ~/$rcfile; then
-        sed -i '' "/FOURIER_PATH/d" ~/$rcfile
-    fi
-    if grep -q 'alias fourier=' ~/$rcfile; then
-        sed -i '' "/alias fourier=/d" ~/$rcfile
-    fi
     echo "export FOURIER_PATH=$FOURIER_PATH" >> ~/$rcfile
     echo "alias fourier='python3 \$FOURIER_PATH/FrAD-Codec/main.py'" >> ~/$rcfile
 done
