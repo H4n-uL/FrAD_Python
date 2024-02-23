2611c010a9020da7dd20b7f1a90326d954c27a7f
h4n-ul <jun061119@gmail.com>
2024-02-23 17:26:01 +0900

    $UPDATE$ :: Update rounding in len / 2048 for compression

--- src/fra/fourier.py
+++ src/fra/fourier.py
@@ -21,6 +21,7 @@ class fourier:
         if lossy:
             v = len(fft_data) // 16
             for c in range(channels):
+                fft_data[c] = np.around(fft_data[c] / (len(data) / 2048)) * (len(data) / 2048)
                 mXbark = psycho.mapping2bark(np.abs(fft_data[c]),W,block_size*2)
                 mTbark = psycho.maskingThresholdBark(mXbark,sprfuncmat,alpha,sample_rate,nfilts)
                 thres =  psycho.mappingfrombark(mTbark,W_inv,block_size*2) / 4 * 1.15**level
