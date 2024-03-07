b60538910d567543f449d836ae978b0a8ed199f8
h4n-ul <jun061119@gmail.com>
2024-03-07 20:05:44 +0900

    wait no

--- src/fra/tools/dsd.py
+++ src/fra/tools/dsd.py
@@ -104,7 +104,7 @@ class dsd:
         cli_width = 40
         i = 0
 
-        dsd_srate = 2822400 // 4
+        dsd_srate = 2822400
         pred_size = os.path.getsize(variables.temp_pcm) // srate * dsd_srate // 64
         try:
             BUFFER_SIZE = 262144 * 8 * channels
