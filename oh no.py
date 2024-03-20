217dffcb7695c280ae854ac1fee4e44686a1b28b
h4n-ul <jun061119@gmail.com>
2024-03-20 14:51:05 +0900

    help

--- oh no.py
+++ oh no.py
@@ -1,6 +1,14 @@
+bits = 16
+channels = 2
 frame_size = 2048
 sample_rate = 48000
 desired_bitrate = 320000
 
-print(f'Bits per frame: {int((desired_bitrate - (sample_rate / frame_size * 256)) / sample_rate * frame_size)} bits')
-print(f'True Bitrate:   {int((desired_bitrate - (sample_rate / frame_size * 256)))} bps')
+uc = frame_size*bits*channels
+tbr = int(desired_bitrate - (sample_rate / frame_size * 256))
+cmp = int(tbr / sample_rate * frame_size)
+
+print(f'Bits per frame uc:  {uc} bits')
+print(f'Bits per frame cmp: {cmp} bits')
+print(f'True Bitrate:       {tbr} bps')
+print(f'Compression rate:   {cmp/uc*100:.3f} %')
