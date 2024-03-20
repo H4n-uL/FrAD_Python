5ce8300b852ce2e9e159f20839e0f053b9f487d2
h4n-ul <jun061119@gmail.com>
2024-03-20 11:52:36 +0900

    god

--- oh no.py
+++ oh no.py
@@ -2,5 +2,5 @@ frame_size = 2048
 sample_rate = 48000
 desired_bitrate = 320000
 
-print(f'Available bits per frame: {int((desired_bitrate - (sample_rate / frame_size * 256)) / sample_rate * frame_size)} bits')
-# 13397 bits for 2048 spf, 48000 Hz, 320 kbps
+print(f'Bits per frame: {int((desired_bitrate - (sample_rate / frame_size * 256)) / sample_rate * frame_size)} bits')
+print(f'True Bitrate:   {int((desired_bitrate - (sample_rate / frame_size * 256)))} bps')
