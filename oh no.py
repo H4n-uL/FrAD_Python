9bf9bd8f33d87d42cecdb2f328409a4a8d506627
h4n-ul <jun061119@gmail.com>
2024-03-20 19:42:04 +0900

    wait

--- oh no.py
+++ oh no.py
@@ -2,7 +2,7 @@ bits = 16
 channels = 2
 frame_size = 2048
 sample_rate = 48000
-desired_bitrate = 8000
+desired_bitrate = 320000
 
 uc = frame_size*bits*channels
 tbr = int(desired_bitrate - (sample_rate / frame_size * 256))
