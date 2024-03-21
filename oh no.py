1393c0af44a8a2ee86202a8d635fca32a56f9c66
h4n-ul <jun061119@gmail.com>
2024-03-21 13:29:44 +0900

    s

--- oh no.py
+++ oh no.py
@@ -6,7 +6,7 @@ desired_bitrate = 320000
 
 uc = frame_size*bits*channels
 tbr = int(desired_bitrate - (sample_rate / frame_size * 256))
-cmp = int(tbr / sample_rate * frame_size // 8 * 8)
+cmp = int(tbr / sample_rate * frame_size // 8 // channels * 8 * channels)
 
 print(f'Bits per frame uncmp:  {uc} bits')
 print(f'Bits per frame cmp:    {cmp} bits')
