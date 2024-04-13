b92dad2b8e3d1a3d7e9765eed290784523865fd7
h4n-ul <jun061119@gmail.com>
2024-04-14 01:10:51 +0900

    $UPDATE$ :: integer update

--- src/FrAD/layers/layer1.py
+++ src/FrAD/layers/layer1.py
@@ -3,10 +3,21 @@ import numpy as np
 from ..tools.psycho import loss
 import zlib
 
-dtypes = {128:'f16',64:'f8',48:'f8',32:'f4',24:'f4',16:'f2',12:'f2'}
+dtypes = {128:'i16',64:'i8',48:'i8',32:'i4',24:'i4',16:'i2',12:'i2'}
 
 fl = [0, 100, 500, 2000, 5000, 10000, 20000, 100000, 500000, np.inf]
 rfs = [4, 5, 7, 8, 6, 4, 3, 1, 0]
+
+def signext_24x(byte, bits, be):
+    padding = int(byte.hex(), base=16) & (1<<be and (bits-1) or 7) and b'\xff' or b'\x00'
+    if be: return padding * (bits//24) + byte
+    else: return byte + padding * (bits//24)
+
+def signext_12(hex_str, be):
+    sign_bit = int(hex_str[be and 0 or 2], base=16) > 7
+    padding = 'f' if sign_bit else '0'
+    if be: return padding + hex_str
+    else: return hex_str[:2] + padding + hex_str[2:]
 
 def rounding(freqs, kwargs):
     dlen = len(freqs[0])
@@ -15,18 +26,18 @@ def rounding(freqs, kwargs):
     for c in range(len(freqs)):
         for i, j in zip(fl[:-1], fl[1:]):
             af = 2**(-rfs[fl.index(i)])
-            freqs[c][fs_list[i]:fs_list[j]] = np.around(freqs[c][fs_list[i]:fs_list[j]] / af) * af
+            freqs[c][fs_list[i]:fs_list[j]] /= af
     return freqs
 
-# def unrounding(freqs, kwargs):
-#     dlen = len(freqs[0])
-#     fs_list = {n:loss.get_range(dlen, kwargs['sample_rate'], n) for n in fl}
+def unrounding(freqs, kwargs):
+    dlen = len(freqs[0])
+    fs_list = {n:loss.get_range(dlen, kwargs['sample_rate'], n) for n in fl}
 
-#     for c in range(len(freqs)):
-#         for i, j in zip(fl[:-1], fl[1:]):
-#             af = 2**(-rfs[fl.index(i)])
-#             freqs[c][fs_list[i]:fs_list[j]] *= af
-#     return freqs
+    for c in range(len(freqs)):
+        for i, j in zip(fl[:-1], fl[1:]):
+            af = 2**(-rfs[fl.index(i)])
+            freqs[c][fs_list[i]:fs_list[j]] *= af
+    return freqs
 
 def analogue(data: np.ndarray, bits: int, channels: int, little_endian: bool, kwargs) -> bytes:
     be = not little_endian
@@ -41,7 +52,7 @@ def analogue(data: np.ndarray, bits: int, channels: int, little_endian: bool, kw
     freqs[1:] -= freqs[0]
 
     # Overflow check & Increasing bit depth
-    while np.max(np.abs(data)) > np.finfo(dtypes[bits]).max:
+    while not (2**(bits-1)-1 >= freqs.any() >= -(2**(bits-1))):
         if bits == 128: raise Exception('Overflow with reaching the max bit depth.')
         bits = {16:24, 24:32, 32:48, 48:64, 64:128}.get(bits, 128)
 
@@ -52,10 +63,10 @@ def analogue(data: np.ndarray, bits: int, channels: int, little_endian: bool, kw
     if bits in [128, 64, 32, 16]:
         pass
     elif bits in [48, 24]:
-        data = b''.join([be and data[i:i+(bits//8)] or data[i+(bits//24):i+(bits//6)] for i in range(0, len(data), bits//6)])
+        data = b''.join([be and data[i+(bits//24):i+(bits//6)] or data[i:i+(bits//8)] for i in range(0, len(data), bits//6)])
     elif bits == 12:
         data = data.hex()
-        data = bytes.fromhex(''.join([be and data[i:i+3] or data[i:i+4][0] + data[i:i+4][2:] for i in range(0, len(data), 4)]))
+        data = bytes.fromhex(''.join([be and data[i+1:i+4] or data[i:i+4][:2] + data[i:i+4][3:] for i in range(0, len(data), 4)]))
     else: raise Exception('Illegal bits value.')
 
     # Deflating
@@ -74,11 +85,10 @@ def digital(data: bytes, fb: int, channels: int, little_endian: bool, *, kwargs)
     # Padding bits
     if bits % 3 != 0: pass
     elif bits in [24, 48]:
-        data = b''.join([be and data[i:i+(bits//8)]+(b'\x00'*(bits//24)) or (b'\x00'*(bits//24))+data[i:i+(bits//8)] for i in range(0, len(data), bits//8)])
+        data = b''.join([signext_24x(data[i:i+(bits//8)], bits, be) for i in range(0, len(data), bits//8)])
     elif bits == 12:
         data = data.hex()
-        if endian == '<': data = ''.join([data[i:i+3][0] + '0' + data[i:i+3][1:] for i in range(0, len(data), 3)])
-        else: data = ''.join([data[i:i+3] + '0' for i in range(0, len(data), 3)])
+        data = ''.join([signext_12(data[i:i+3], be) for i in range(0, len(data), 3)])
         data = bytes.fromhex(data)
     else:
         raise Exception('Illegal bits value.')
@@ -89,6 +99,7 @@ def digital(data: bytes, fb: int, channels: int, little_endian: bool, *, kwargs)
 
     # Removing potential Infinities and Non-numbers
     freqs = np.where(np.isnan(freqs) | np.isinf(freqs), 0, freqs)
+    freqs = unrounding(freqs, kwargs)
     # Inter-channel reconstruction
     freqs[1:] += freqs[0]
     freqs = np.sign(freqs) * np.abs(freqs)**(4/3)
