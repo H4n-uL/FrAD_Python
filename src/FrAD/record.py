b7479f4d02902d8d906e8cf4c8136633189e7c43
h4n-ul <jun061119@gmail.com>
2024-05-28 15:39:41 +0900

    $UPDATE$ :: Relying recorder on Encoder

--- src/FrAD/record.py
+++ src/FrAD/record.py
@@ -1,4 +1,5 @@
 from .fourier import fourier
+from .encoder import encode
 import numpy as np
 import os, struct, sys, zlib
 import sounddevice as sd
@@ -14,7 +15,7 @@ class recorder:
         channels = kwargs.get('chnl', None)
 
         # FrAD specifications
-        bit_depth = kwargs.get('bits', 24)
+        bit_depth = kwargs.get('bits', 16)
         fsize: int = kwargs.get('fsize', 2048)
         little_endian: bool = kwargs.get('le', False)
         profile: int = kwargs.get('prf', 0)
@@ -36,12 +37,6 @@ class recorder:
         if fsize % 2 != 0: print('Sample size must be multiple of 2.'); sys.exit()
         if not 20 >= loss_level >= 0: print(f'Invalid compression level: {loss_level} Lossy compression level should be between 0 and 20.'); sys.exit()
         if profile == 2 and fsize%8!=0: print(f'Invalid frame size {fsize} Frame size should be multiple of 8 for Profile 2.'); sys.exit()
-        if profile in [1, 2]:
-            print('\033[1m!!!Warning!!!\033[0m\nFourier Analogue-in-Digital is designed to be an uncompressed archival codec. Compression increases the difficulty of decoding and makes data very fragile, making any minor damage likely to destroy the entire frame. Proceed? (Y/N)')
-            while True:
-                x = input('> ').lower()
-                if x == 'y': break
-                if x == 'n': sys.exit('Aborted.')
 
         print('Please enter your recording device ID from below.')
         for ind, dev in enumerate(sd.query_devices()):
@@ -68,50 +63,26 @@ class recorder:
         ecc_dsize, ecc_codesize = int(ecc_sizes[0]), int(ecc_sizes[1])
         print('Recording...')
         open(file_path, 'wb').write(headb.uilder(meta, img))
+        prev = np.array([])
 
         record = sd.InputStream(samplerate=smprate, channels=channels, device=hw, dtype=np.float32)
         record.start()
         with open(file_path, 'ab') as f:
             while True:
                 try:
-                    data = record.read(fsize)[0]
-                    flen = len(data)
-                    data, _, chnl, bf = fourier.analogue(data, bit_depth, channels, little_endian, profile=profile, smprate=smprate, level=loss_level)
+                    rlen = fsize
+                    while rlen < len(prev): rlen += 128
+                    # Overlap
+                    if profile in [1, 2] and len(prev) != 0: rlen -= len(prev)
+                    data = record.read(rlen)[0]
+                    data, prev = encode.overlap(data, prev, fsize=fsize, chnl=channels, profile=profile)
+                    frame, _, chnl, bf = fourier.analogue(data, bit_depth, channels, little_endian, profile=profile, smprate=smprate, level=loss_level)
 
                     # Applying ECC (This will make encoding hundreds of times slower)
-                    if apply_ecc: data = ecc.encode(data, ecc_dsize, ecc_codesize)
+                    if apply_ecc: frame = ecc.encode(frame, ecc_dsize, ecc_codesize)
 
                     efb = headb.encode_efb(profile, apply_ecc, little_endian, bf)
-                    data = bytes(
-                        #-- 0x00 ~ 0x0f --#
-                            # Frame Signature
-                            b'\xff\xd0\xd2\x97' +
-
-                            # Segment length(Processed)
-                            struct.pack('>I', len(data)) +
-
-                            efb + # EFB
-                            struct.pack('>B', chnl - 1) +                         # Channels
-                            struct.pack('>B', ecc_dsize if apply_ecc else 0) +    # ECC DSize
-                            struct.pack('>B', ecc_codesize if apply_ecc else 0) + # ECC code size
-
-                            struct.pack('>I', smprate) +                       # Sample Rate
-
-                        #-- 0x10 ~ 0x1f --#
-                            b'\x00'*8 +
-
-                            # Samples in a frame per channel
-                            struct.pack('>I', flen) +
-
-                            # ISO 3309 CRC32
-                            struct.pack('>I', zlib.crc32(data)) +
-
-                        #-- Data --#
-                        data
-                    )
-
-                    # WRITE
-                    f.write(data)
+                    encode.write_frame(f, frame, chnl, smprate, efb, (ecc_dsize, ecc_codesize), len(data))
 
                 except KeyboardInterrupt:
                     break
