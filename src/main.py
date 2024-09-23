5e13e274fa262770f5727a6c518907c3176e312b
h4n-ul <jun061119@gmail.com>
2024-09-23 22:04:26 +0900

    DOC :: Updated help pages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from tools import cli
 import encoder, decoder, repairer, header
-import sys
+import os, sys
 
-'''               Fourier Analogue-in-Digital Master encoder/decoder
-                             Original Author - HaמuL
-'''
+PATH_ABSOLUTE = os.path.dirname(os.path.abspath(__file__))
+BANNER = \
+'               Fourier Analogue-in-Digital Master encoder/decoder\n' + \
+'                             Original Author - HaמuL\n'
 
 def main():
+    executable = f'python3 {os.path.basename(sys.argv[0])}' if sys.argv[0].endswith('.py') else sys.argv[0]
     ACTION, METAACTION, INPUT, PARAMS = cli.parse(sys.argv)
 
     if ACTION in cli.ENCODE_OPT:
@@ -19,5 +21,22 @@ def main():
         repairer.repair(INPUT, PARAMS)
     elif ACTION in cli.METADATA_OPT:
         header.modify(INPUT, METAACTION, PARAMS)
+    elif ACTION in cli.HELP_OPT:
+        print(BANNER)
+        helpname = 'general'
+        if INPUT in cli.ENCODE_OPT:       helpname = 'encode'
+        elif INPUT in cli.DECODE_OPT:     helpname = 'decode'
+        elif INPUT in cli.REPAIR_OPT:     helpname = 'repair'
+        elif INPUT in cli.PLAY_OPT:       helpname = 'play'
+        elif INPUT in cli.METADATA_OPT:   helpname = 'metadata'
+        elif INPUT in cli.JSONMETA_OPT:   helpname = 'jsonmeta'
+        elif INPUT in cli.VORBISMETA_OPT: helpname = 'vorbismeta'
+        elif INPUT in cli.PROFILES_OPT:   helpname = 'profiles'
+        print(open(f'{PATH_ABSOLUTE}/help/{helpname}.txt', 'r').read().replace(b'{frad}'.decode(), executable))
+        print()
+    else:
+        print('Fourier Analogue-in-Digital Rust Reference', file=sys.stderr)
+        print(f'Abstract syntax: {executable} [encode|decode|repair] <input> [kwargs...]', file=sys.stderr)
+        print(f'type `{executable} help` to get help.', file=sys.stderr)
 
 if __name__ == '__main__': main()
