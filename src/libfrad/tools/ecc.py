25a67aeddb91ed4e5aab9cf22b3bc5264ca1d708
h4n-ul <jun061119@gmail.com>
2024-10-07 03:03:08 +0900

    UPDATE :: Updated un-ecc logic

--- src/libfrad/tools/ecc.py
+++ src/libfrad/tools/ecc.py
@@ -1,13 +1,5 @@
 from reedsolo import RSCodec, ReedSolomonError
 
-def unecc(data: bytes, ecc_dsize: int, ecc_codesize: int) -> bytes:
-    blocksize = ecc_dsize + ecc_codesize
-
-    block = bytearray()
-    for i in range(0, len(data), blocksize):
-        block.extend(data[i:i+blocksize][:-ecc_codesize])
-    return bytes(block)
-
 def split_data(data: bytes, chunk_size: int):
     for i in range(0, len(data), chunk_size): yield data[i:i+chunk_size]
 
@@ -20,15 +12,14 @@ def encode(data: bytes, ecc_dsize: int, ecc_codesize: int) -> bytes:
     return data
 
 def decode(data: bytes, ecc_dsize: int, ecc_codesize: int, repair: bool) -> bytes:
-    if repair:
-        blocksize = ecc_dsize + ecc_codesize
-        rs = RSCodec(ecc_codesize, blocksize)
+    blocksize = ecc_dsize + ecc_codesize
+    rs = RSCodec(ecc_codesize, blocksize)
 
-        decoded_chunk = []
-        for chunk in split_data(data, blocksize):
+    decoded_chunk = []
+    for chunk in split_data(data, blocksize):
+        if repair:
             try: decoded_chunk.append(bytes(rs.decode(chunk)[0]))
             except ReedSolomonError as _: decoded_chunk.append(b'\x00'*ecc_dsize)
+        else: decoded_chunk.append(chunk[:len(chunk)-ecc_codesize])
 
-        return b''.join(decoded_chunk)
-    else:
-        return unecc(data, ecc_dsize, ecc_codesize)
+    return b''.join(decoded_chunk)
