b47c7b1749a1936debb51ca141270d23da75476d
H4n-uL <jun061119@gmail.com>
2024-12-11 16:05:02 +0900

    DOC :: Updated constants

--- src/libfrad/fourier/tools/p1tools.py
+++ src/libfrad/fourier/tools/p1tools.py
@@ -1,29 +1,27 @@
 import numpy as np
 import struct
 
-# Modified Opus Subbands
-MOS =  (0,     200,   400,   600,   800,   1000,  1200,  1400,
+MODIFIED_OPUS_SUBBANDS =  (0,     200,   400,   600,   800,   1000,  1200,  1400,
         1600,  2000,  2400,  2800,  3200,  4000,  4800,  5600,
         6800,  8000,  9600,  12000, 15600, 20000, 24000, 28800,
         34400, 40800, 48000, (2**32)-1)
 
-subbands = len(MOS) - 1
-rndint = lambda x: int(x+0.5)
-spread_alpha = 0.8
-quant_alpha = 0.75
+SUBBANDS = len(MODIFIED_OPUS_SUBBANDS) - 1
+SPREAD_ALPHA = 0.8
+QUANT_ALPHA = 0.75
 
 def get_bin_range(dlen: int, srate: int, subband_index: int) -> slice:
-    return slice(rndint(dlen/(srate/2)*MOS[subband_index]), rndint(dlen/(srate/2)*MOS[subband_index+1]))
+    return slice(round(dlen/(srate/2)*MODIFIED_OPUS_SUBBANDS[subband_index]), round(dlen/(srate/2)*MODIFIED_OPUS_SUBBANDS[subband_index+1]))
 
 def mask_thres_mos(freqs: np.ndarray, srate: int, bit_depth: int, loss_level: float, alpha: float) -> np.ndarray:
     freqs = np.abs(freqs)
     pcm_scale = 1 << (bit_depth-1)
-    thres = np.zeros(subbands)
-    for i in range(subbands):
+    thres = np.zeros(SUBBANDS)
+    for i in range(SUBBANDS):
         subfreqs = freqs[get_bin_range(len(freqs), srate, i)]
         if len(subfreqs) == 0: break
 
-        f = (MOS[i] + MOS[i+1]) / 2
+        f = (MODIFIED_OPUS_SUBBANDS[i] + MODIFIED_OPUS_SUBBANDS[i+1]) / 2
         absolute_hearing_threshold = 10.0**(
             (3.64*(f/1000.)**-0.8 - 6.5*np.exp(-0.6*(f/1000.-3.3)**2.) + 1e-3*((f/1000.)**4.))/20
         ) / pcm_scale
@@ -35,14 +33,14 @@ def mask_thres_mos(freqs: np.ndarray, srate: int, bit_depth: int, loss_level: fl
 
 def mapping_from_opus(mapped_thres, freqs_len, srate):
     thres = np.zeros(freqs_len)
-    for i in range(subbands-1):
+    for i in range(SUBBANDS-1):
         start = min(get_bin_range(freqs_len, srate, i).start, freqs_len)
         end = min(get_bin_range(freqs_len, srate, i+1).start, freqs_len)
         thres[start:end] = np.linspace(mapped_thres[i], mapped_thres[i+1], end-start)
     return thres
 
-def quant(x: np.ndarray) -> np.ndarray: return np.sign(x) * np.abs(x)**quant_alpha
-def dequant(x: np.ndarray) -> np.ndarray: return np.sign(x) * np.abs(x)**(1/quant_alpha)
+def quant(x: np.ndarray) -> np.ndarray: return np.sign(x) * np.abs(x)**QUANT_ALPHA
+def dequant(x: np.ndarray) -> np.ndarray: return np.sign(x) * np.abs(x)**(1/QUANT_ALPHA)
 
 bitstr2bytes = lambda bstr: bytes(int(bstr[i:i+8].ljust(8, '0'), 2) for i in range(0, len(bstr), 8))
 bytes2bitstr = lambda b: ''.join(f'{byte:08b}' for byte in b)
